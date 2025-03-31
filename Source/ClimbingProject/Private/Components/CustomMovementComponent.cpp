663f5d3e5f746aac6c275b066d4ac6828dcebbc5
Martyna Cichosz <martynacichosz2@gmail.com>
2025-03-31 10:37:09 +0200

    Capsule Trace

--- Source/ClimbingProject/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingProject/Private/Components/CustomMovementComponent.cpp
@@ -1,5 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Components/CustomMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+#pragma region ClimbTraces
+
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false)
+{
+	TArray<FHitResult> OutHits;
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutHits,
+		false
+	);
+	return OutHits;
+}
+
+#pragma endregion
