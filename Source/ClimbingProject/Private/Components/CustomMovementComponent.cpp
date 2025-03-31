2df1413eda9acaab8b43033cf8901c1ffdb175e4
Martyna Cichosz <96289919+zielonoludek@users.noreply.github.com>
2025-03-31 11:20:41 +0200

    Update CustomMovementComponent.cpp

--- Source/ClimbingProject/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingProject/Private/Components/CustomMovementComponent.cpp
@@ -1,11 +1,19 @@
 #include "Components/CustomMovementComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 
+void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime,TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+}
+
 #pragma region ClimbTraces
 
-TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false)
-{
-	TArray<FHitResult> OutHits;
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector & Start, const FVector & End, bool bShowDebugShape)
+{	
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+
 	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
 		this,
 		Start,
@@ -15,11 +23,25 @@ TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const F
 		ClimbableSurfaceTraceTypes,
 		false,
 		TArray<AActor*>(),
-		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
-		OutHits,
+		bShowDebugShape? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResults,
 		false
 	);
-	return OutHits;
+
+	return OutCapsuleTraceHitResults;
+}
+
+#pragma endregion
+
+#pragma region ClimbCore
+
+void UCustomMovementComponent::TraceClimbableSurfaces()
+{	
+	const FVector StartOffset = UpdatedComponent->GetForwardVector() * 30.f;
+	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector();
+
+	DoCapsuleTraceMultiByObject(Start,End,true);
 }
 
 #pragma endregion
