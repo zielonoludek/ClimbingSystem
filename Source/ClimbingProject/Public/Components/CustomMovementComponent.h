e84af3f4d2521abe55436e5e9bc755b626b00ab9
Martyna Cichosz <martynacichosz2@gmail.com>
2025-04-24 16:54:06 +0200

    eye trace

--- Source/ClimbingProject/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingProject/Public/Components/CustomMovementComponent.h
@@ -20,12 +20,14 @@ private:
 #pragma region ClimbTraces
 	
 	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start,const FVector& End,bool bShowDebugShape = false);
+	FHitResult DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
 
 #pragma endregion
 
 #pragma region ClimbCore
 	
 	void TraceClimbableSurfaces();
+	void TraceFromEyeHeight(float TraceDistance, float TraceStartOffset = 0.f);
 
 #pragma endregion
 
